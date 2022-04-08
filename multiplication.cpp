b780435fd7ed4c4a0dfe2aa17928a6e160ca8d17
Sanmitha L <84409269+sanmitha-23@users.noreply.github.com>
2022-04-08 23:25:20 +0530

    Created a program to compute multiplication
    This program takes two floating point numbers and computes their product and display the result.

---
+++ multiplication.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,prod;
+    cout<<"Enter the two numbers:\n";
+    cin>>a>>b;
+
+    prod=a*b;
+    cout<<"Product = "<<prod;
+
+    return 0;
+
+}
